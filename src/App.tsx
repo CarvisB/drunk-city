9247159d8ea2e4e4f3a7b833b83eeae012e57734
Carvis Banks <carvisbanks@gmail.com>
2025-12-13 13:14:20 -0500

    Added a second home route: /drunk-city/

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/drunk-city" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/about" element={<About />} />
       </Routes>
